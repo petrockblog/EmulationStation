9034a8ce6dc7f06c0b0e6a3956e27eb7614bbbf1
Daniel Mota <AndroidKing@gmail.com>
2017-05-04 11:23:01 -0400

    #124: Adding sorting options for the number of players, release date, genre, developer and publisher.

--- es-app/src/FileSorts.cpp
+++ es-app/src/FileSorts.cpp
@@ -13,7 +13,22 @@ namespace FileSorts
 		FileData::SortType(&compareTimesPlayed, false, "times played, descending"),
 
 		FileData::SortType(&compareLastPlayed, true, "last played, ascending"),
-		FileData::SortType(&compareLastPlayed, false, "last played, descending")
+		FileData::SortType(&compareLastPlayed, false, "last played, descending"),
+
+		FileData::SortType(&compareNumPlayers, true, "number players, ascending"),
+		FileData::SortType(&compareNumPlayers, false, "number players, descending"),
+
+		FileData::SortType(&compareReleaseDate, true, "release date, ascending"),
+		FileData::SortType(&compareReleaseDate, false, "release date, descending"),
+
+		FileData::SortType(&compareGenre, true, "genre, ascending"),
+		FileData::SortType(&compareGenre, false, "genre, descending"),
+
+		FileData::SortType(&compareDeveloper, true, "developer, ascending"),
+		FileData::SortType(&compareDeveloper, false, "developer, descending"),
+
+		FileData::SortType(&comparePublisher, true, "publisher, ascending"),
+		FileData::SortType(&comparePublisher, false, "publisher, descending")
 	};
 
 	const std::vector<FileData::SortType> SortTypes(typesArr, typesArr + sizeof(typesArr)/sizeof(typesArr[0]));
@@ -68,5 +83,81 @@ namespace FileSorts
 		}
 
 		return false;
+	}
+
+	bool compareNumPlayers(const FileData* file1, const FileData* file2)
+	{
+		//only games have players metadata
+		if (file1->metadata.getType() == GAME_METADATA && file2->metadata.getType() == GAME_METADATA)
+		{
+			return (file1)->metadata.getInt("players") < (file2)->metadata.getInt("players");
+		}
+
+		return false;
+	}
+
+	bool compareReleaseDate(const FileData* file1, const FileData* file2)
+	{
+		//only games have releasedate metadata
+		if (file1->metadata.getType() == GAME_METADATA && file2->metadata.getType() == GAME_METADATA)
+		{
+			return (file1)->metadata.getTime("releasedate") < (file2)->metadata.getTime("releasedate");
+		}
+
+		return false;
+	}
+
+	bool compareGenre(const FileData* file1, const FileData* file2)
+	{
+		std::string genre1 = file1->metadata.get("genre");
+		std::string genre2 = file2->metadata.get("genre");
+
+		//min of genre1/genre2 .length()s
+		unsigned int count = genre1.length() > genre2.length() ? genre2.length() : genre1.length();
+		for (unsigned int i = 0; i < count; i++)
+		{
+			if (toupper(genre1[i]) != toupper(genre2[i]))
+			{
+				return toupper(genre1[i]) < toupper(genre2[i]);
+			}
+		}
+
+		return genre1.length() < genre2.length();
+	}
+
+	bool compareDeveloper(const FileData* file1, const FileData* file2)
+	{
+		std::string developer1 = file1->metadata.get("developer");
+		std::string developer2 = file2->metadata.get("developer");
+
+		//min of developer1/developer2 .length()s
+		unsigned int count = developer1.length() > developer2.length() ? developer2.length() : developer1.length();
+		for (unsigned int i = 0; i < count; i++)
+		{
+			if (toupper(developer1[i]) != toupper(developer2[i]))
+			{
+				return toupper(developer1[i]) < toupper(developer2[i]);
+			}
+		}
+
+		return developer1.length() < developer2.length();
+	}
+
+	bool comparePublisher(const FileData* file1, const FileData* file2)
+	{
+		std::string publisher1 = file1->metadata.get("publisher");
+		std::string publisher2 = file2->metadata.get("publisher");
+
+		//min of publisher1/publisher2 .length()s
+		unsigned int count = publisher1.length() > publisher2.length() ? publisher2.length() : publisher1.length();
+		for (unsigned int i = 0; i < count; i++)
+		{
+			if (toupper(publisher1[i]) != toupper(publisher2[i]))
+			{
+				return toupper(publisher1[i]) < toupper(publisher2[i]);
+			}
+		}
+
+		return publisher1.length() < publisher2.length();
 	}
 };
