fcf730ff899e5aebbe842a780e55a22ff00c60b7
Daniel Mota <AndroidKing@gmail.com>
2017-05-06 00:55:57 -0400

    #124: Using string compare for publisher, developer, genre and file name.

--- es-app/src/FileSorts.cpp
+++ es-app/src/FileSorts.cpp
@@ -36,20 +36,7 @@ namespace FileSorts
 	//returns if file1 should come before file2
 	bool compareFileName(const FileData* file1, const FileData* file2)
 	{
-		std::string name1 = file1->getName();
-		std::string name2 = file2->getName();
-
-		//min of name1/name2 .length()s
-		unsigned int count = name1.length() > name2.length() ? name2.length() : name1.length();
-		for(unsigned int i = 0; i < count; i++)
-		{
-			if(toupper(name1[i]) != toupper(name2[i]))
-			{
-				return toupper(name1[i]) < toupper(name2[i]);
-			}
-		}
-
-		return name1.length() < name2.length();
+		return file1->getName().compare(file2->getName()) < 0;
 	}
 
 	bool compareRating(const FileData* file1, const FileData* file2)
@@ -109,55 +96,16 @@ namespace FileSorts
 
 	bool compareGenre(const FileData* file1, const FileData* file2)
 	{
-		std::string genre1 = file1->metadata.get("genre");
-		std::string genre2 = file2->metadata.get("genre");
-
-		//min of genre1/genre2 .length()s
-		unsigned int count = genre1.length() > genre2.length() ? genre2.length() : genre1.length();
-		for (unsigned int i = 0; i < count; i++)
-		{
-			if (toupper(genre1[i]) != toupper(genre2[i]))
-			{
-				return toupper(genre1[i]) < toupper(genre2[i]);
-			}
-		}
-
-		return genre1.length() < genre2.length();
+		return file1->metadata.get("genre").compare(file2->metadata.get("genre")) < 0;
 	}
 
 	bool compareDeveloper(const FileData* file1, const FileData* file2)
 	{
-		std::string developer1 = file1->metadata.get("developer");
-		std::string developer2 = file2->metadata.get("developer");
-
-		//min of developer1/developer2 .length()s
-		unsigned int count = developer1.length() > developer2.length() ? developer2.length() : developer1.length();
-		for (unsigned int i = 0; i < count; i++)
-		{
-			if (toupper(developer1[i]) != toupper(developer2[i]))
-			{
-				return toupper(developer1[i]) < toupper(developer2[i]);
-			}
-		}
-
-		return developer1.length() < developer2.length();
+		return file1->metadata.get("developer").compare(file2->metadata.get("developer")) < 0;
 	}
 
 	bool comparePublisher(const FileData* file1, const FileData* file2)
 	{
-		std::string publisher1 = file1->metadata.get("publisher");
-		std::string publisher2 = file2->metadata.get("publisher");
-
-		//min of publisher1/publisher2 .length()s
-		unsigned int count = publisher1.length() > publisher2.length() ? publisher2.length() : publisher1.length();
-		for (unsigned int i = 0; i < count; i++)
-		{
-			if (toupper(publisher1[i]) != toupper(publisher2[i]))
-			{
-				return toupper(publisher1[i]) < toupper(publisher2[i]);
-			}
-		}
-
-		return publisher1.length() < publisher2.length();
+		return file1->metadata.get("publisher").compare(file2->metadata.get("publisher")) < 0;
 	}
 };
