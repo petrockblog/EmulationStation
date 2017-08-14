09bfe2f487870fd8323947bd87c269cc491c7d81
Benjamin D <benjdero@gmail.com>
2017-08-15 01:32:21 +0200

    Add support for plural

--- es-core/src/Localization.h
+++ es-core/src/Localization.h
@@ -3,3 +3,4 @@
 #include <boost/locale.hpp>
 
 #define _(STRING) boost::locale::gettext(STRING)
+#define __(SINGULAR, PLURAL, COUNT) boost::locale::format(boost::locale::ngettext(SINGULAR, PLURAL, COUNT)) % COUNT
