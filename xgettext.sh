52c62f939ac3667a72c19731c29efa920b8131b1
Benjamin D <benjdero@gmail.com>
2017-08-15 02:05:58 +0200

    French traduction nearly complete

--- xgettext.sh
+++ xgettext.sh
@@ -1,1 +1,1 @@
-find . -iname "*.cpp" | xargs xgettext --from-code=UTF-8 --default-domain=emulationstation --keyword=_ --add-comments --sort-output -o locale/emulationstation.pot
+find . -iname "*.cpp" | xargs xgettext --from-code=UTF-8 --default-domain=emulationstation --keyword=_ --add-comments -o locale/emulationstation.pot
